9cf574d3f459ad9e75e93d3730308fdae1285f31
Thomas Broadley <buriedunderbooks@hotmail.com>
2022-04-09 10:18:49 -0400

    Add tests for parseUnit

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-const { nextUnit, 
+const { expect } = require('chai');
+
+const { parseUnit, randUnitPosition, findNextAttackPosition } = require('./index');
+
+describe("parseUnit", () => {
+	it("parses a basic unit", () => {
+		expect(parseUnit("1")).to.deep.equal({ attacksLeft: 1, ranged: false, resummon: false });
+	});
+
+	it("parses a ranged unit", () => {
+		expect(parseUnit("2R")).to.deep.equal({ attacksLeft: 2, ranged: true, resummon: false });
+	});
+
+	it("parses a resummon unit", () => {
+		expect(parseUnit("3S")).to.deep.equal({ attacksLeft: 3, ranged: false, resummon: true });
+	});
+
+	it("parses an empty slot", () => {
+		expect(parseUnit("0")).to.deep.equal({ attacksLeft: 0, ranged: false, resummon: false });
+	});
+});
